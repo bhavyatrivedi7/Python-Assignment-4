166de44871fbdee588f60d5cce5bbe33fa5367cf
Bhavya Trivedi <60940701+bhavyatrivedi7@users.noreply.github.com>
2020-02-19 02:30:10 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,7 +1,5 @@
 dic={1:"bhavya",2:"tanuj",3:"avilun",4:"moor",5:"gor"}
 dic1={1:"bhavya",2:"tanuj",3:"avilun"}
-dic3={1:23,2:25,3:27,4:30}
-dic4={1:24,2:25,3:28,4:30}
 del dic[5]
 print(dic)
 #cmp() is not supported in python 3
